82b52645f4985841e6f79ea9c16cfb14434f5510
Artuur Alexandrov <naturahxq@gmail.com>
2024-02-12 16:56:07 +0300

    Delete Function

--- src/components/OutputProjects/OutputProjects.tsx
+++ src/components/OutputProjects/OutputProjects.tsx
@@ -1,7 +1,6 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import style from "./OutputProjects.module.css";
 import Button from "../Button/Button";
-
 
 interface OutputProjectsProps {
   title: string;
@@ -9,14 +8,32 @@ interface OutputProjectsProps {
 }
 
 const OutputProjects: FC<OutputProjectsProps> = ({ title, description }) => {
+  const [projectTitle, setProjectTitle] = useState<string>(title);
+  const [projectDescription, setDescription] = useState<string>(description);
+
+  const handleDelete = () => {
+    setProjectTitle("");
+    setDescription("");
+  };
 
   return (
     <div className={style.display}>
       <div className={style.text}>{title}</div>
       <div className={style.subtext}>{description}</div>
-      <Button style={{background: 'red', color: 'white', position: 'absolute', bottom: '0', marginLeft: '50px'}}>Удалить</Button>
+      <Button
+        onClick={handleDelete}
+        style={{
+          background: "red",
+          color: "white",
+          position: "absolute",
+          bottom: "0",
+          marginLeft: "50px",
+        }}
+      >
+        Удалить
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default OutputProjects
+export default OutputProjects;
